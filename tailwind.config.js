eb634c787e78a3530e9d8a7608296fe7fad0515b
m-arti <marti.samuel1@gmail.com>
2021-03-15 00:37:59 +0000

    update style

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,11 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   purge: [],
   theme: {
-  
+
     extend: {
       fontFamily: {
         sans: ['SF Pro Display', ...defaultTheme.fontFamily.sans],
-        serif: ['Times New Roman', ...defaultTheme.fontFamily.serif],
+        serif: ["Times New Roman", ...defaultTheme.fontFamily.serif],
       },
     },
   },
