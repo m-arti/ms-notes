3dc6bef41d6a560be04b93de2cf66c25360493e7
m-arti <marti.samuel1@gmail.com>
2021-04-02 07:57:13 +0100

    update notes

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,6 @@ module.exports = {
         sans: ['SF Pro Display', ...defaultTheme.fontFamily.sans],
         serif: ["Times New Roman", ...defaultTheme.fontFamily.serif],
       },
-
-      backgroundImage: theme => ({
-         'rita-dove': "url('https://www.wolframcloud.com/obj/mar/header-rita-dove')",
-        })
     },
   },
   variants: {},
