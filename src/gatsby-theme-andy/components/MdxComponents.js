b2e59104bbac11f737f4506f31e9fe1ad28c80b4
m-arti <marti.samuel1@gmail.com>
2021-04-04 23:52:37 +0100

    Update MdxComponents.js

--- src/gatsby-theme-andy/components/MdxComponents.js
+++ src/gatsby-theme-andy/components/MdxComponents.js
@@ -65,7 +65,8 @@ const AnchorTag = ({ href, popups = {}, noPopups = false, ...restProps }) => {
 
 const NoteTag = ({ children, color }) => (
   <div
-    className={`notetag antialiased bg-${color}-200 text-${color}-700 py-1 px-2 mb-5 mr-2 text-xs font-normal rounded-md inline-block`}
+    className=
+    {`notetag antialiased bg-${color}-200 text-black py-1 px-2 mb-5 mr-2 text-xs font-normal rounded-md inline-block`}
   >
     {children}
   </div>
