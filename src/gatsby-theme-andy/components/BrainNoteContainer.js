2dbf972040b49b6c0aafad9fa98de0bd58fd0a4b
m-arti <marti.samuel1@gmail.com>
2021-04-05 17:15:43 +0100

    Update BrainNoteContainer.js

--- src/gatsby-theme-andy/components/BrainNoteContainer.js
+++ src/gatsby-theme-andy/components/BrainNoteContainer.js
@@ -43,7 +43,7 @@ const StackedPageWrapper = ({
         left: PAD * i,
         right: - NOTE_WIDTH + PAD,
         width: NOTE_WIDTH,
-        backgroundColor: `${ ColourMode()===true ? 'rgba(0, 0, 0, 1)' : 'rgba(255, 255, 255, 1)'}`,
+        backgroundColor: `${ ColourMode() ? 'rgba(0, 0, 0, 1)' : 'rgba(255, 255, 255, 1)'}`,
         boxShadow:`${ ColourMode() && overlay ? '0 80px 15px -3px rgba(103, 128, 159, .3), 0 4px 4px -2px rgba(103, 128, 159, .3)' : ''} ${ !ColourMode() && overlay ? '0 80px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 4px -2px rgba(0, 0, 0, 0.05)' : ''}`
       }}
     >
