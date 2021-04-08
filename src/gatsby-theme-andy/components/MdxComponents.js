4374dfda217089a784c6a20a275a3d2b4f54d151
m-arti <marti.samuel1@gmail.com>
2021-04-08 04:45:28 +0100

    update site

--- src/gatsby-theme-andy/components/MdxComponents.js
+++ src/gatsby-theme-andy/components/MdxComponents.js
@@ -58,7 +58,6 @@ const NoteTag = ({ children, color }) => (
 const nbStyles = `dark:bg-black w-full pb-6 align-middle border-t-2 border-gray-300 hover:border-t-2 hover:border-orange-400 transition ease-in-out duration-500`;
 
 // Embed Wolfram Notebooks in notes (code from: https://wolfr.am/SHFaaKUP)
-
 class WolframNotebook extends React.Component {
 
   constructor () {
@@ -67,19 +66,15 @@ class WolframNotebook extends React.Component {
   }
 
   async componentDidMount() {
-
     if (typeof window !== 'undefined') {
       this.embedding = await WolframNotebookEmbedder.embed(this.props.url, this.el, this.props.attributes);
     }
-
   }
 
   componentWillUnmount() {
-
     if (typeof window !== 'undefined') {
       this.embedding.then(nb => nb.detach());
     }
-
   }
 
   render() {
