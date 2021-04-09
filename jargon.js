29a059f620d2b447ebccb8b9d3fd11100b846aba
m-arti <marti.samuel1@gmail.com>
2021-04-09 04:35:44 +0100

    update site

--- jargon.js
+++ jargon.js
@@ -2,7 +2,7 @@ const jargon = {
 
   hypercubegraph: "<b><code>HypercubeGraph</code></b> <code>HypercubeGraph[n]</code> gives the n-dimensional hypercube graph <katex-expression expression='Q_n'></katex-expression>. See <a href='https://reference.wolfram.com/language/ref/HypercubeGraph.html'>Wolfram Documentation</a>.",
 
-  tesseract: "<b>Tesseract</b> The tesseract is the hypercube in <katex-expression expression='\R^4'></katex-expression>, also called the 8-cell or octachoron. See <a href='https://mathworld.wolfram.com/Tesseract.html'>Mathworld entry</a>.",
+  tesseract: "<b>Tesseract</b> The tesseract is the hypercube in <katex-expression expression='\Reals^4'></katex-expression>, also called the 8-cell or octachoron. See <a href='https://mathworld.wolfram.com/Tesseract.html'>Mathworld entry</a>.",
 
   // add more defs
 
