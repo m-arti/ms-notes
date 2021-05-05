8c7a9df0ad9fe4e15f9625f4933a4f750185f8db
m-arti <marti.samuel1@gmail.com>
2021-05-05 04:13:20 +0100

    update notes & style

--- jargon.js
+++ jargon.js
@@ -3,9 +3,7 @@ const jargon = {
   hypercubegraph: "<b><code>HypercubeGraph</code></b> <code>HypercubeGraph[n]</code> gives the n-dimensional hypercube graph <katex-expression expression='Q_n'></katex-expression>. See <a href='https://reference.wolfram.com/language/ref/HypercubeGraph.html'>Wolfram Documentation</a>.",
 
   tesseract: "<b>Tesseract</b> The tesseract is the hypercube in <katex-expression expression='ℝ^4'></katex-expression>, also called the 8-cell or octachoron. See <a href='https://mathworld.wolfram.com/Tesseract.html'>Mathworld entry</a>.",
-
-  // add more defs
-
+  
 }
 
 module.exports = jargon;
