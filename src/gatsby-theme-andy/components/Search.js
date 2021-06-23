fd2fe957ef23c20d6df3d4135ca703f5d6c8f9b7
m-arti <marti.samuel1@gmail.com>
2021-06-23 16:42:56 +0100

    Update Search.js

--- src/gatsby-theme-andy/components/Search.js
+++ src/gatsby-theme-andy/components/Search.js
@@ -38,7 +38,7 @@ class Search extends Component {
     return (
       <div className={this.props.classNames}>
         <input
-          className="search__input"
+          className="search__input bg-white"
           type="text"
           onChange={this.search}
           placeholder={'> Search...'}
