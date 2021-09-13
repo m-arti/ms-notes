b8a454cb3344787352b4cf7fdf26de4317ddac61
m-arti <marti.samuel1@gmail.com>
2021-09-13 01:22:11 +0100

    Update GraphButton.js

--- src/gatsby-theme-andy/components/GraphButton.js
+++ src/gatsby-theme-andy/components/GraphButton.js
@@ -14,10 +14,10 @@ const GraphButton = () => {
   return (
     <React.Fragment>
       <button
-        style={{ fontSize: '1.73rem', lineHeight: '20px' }}
+        style={{ fontSize: '1.75rem', textAlign: 'center', lineHeight: '20px' }}
         title="Show graph of notes"
         aria-label="Show graph of notes"
-        className="h-8 w-8 pb-1.5 overflow-hidden text-center transition duration-500 ease-in-out opacity-80 hover:opacity-100"
+        className="w-8 pb-1.5 overflow-hidden text-center transition duration-500 ease-in-out opacity-80 hover:opacity-100"
         onClick={() => {
           setGraphState("maximized");
           if (graphState !== "maximized") play();
