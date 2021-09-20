341f46dc61de43d0a9d422a746dfe9f6b26a5ab0
m-arti <marti.samuel1@gmail.com>
2021-09-20 19:38:39 +0100

    Update GraphVisualisation.js

--- src/gatsby-theme-andy/components/GraphVisualisation.js
+++ src/gatsby-theme-andy/components/GraphVisualisation.js
@@ -28,8 +28,8 @@ const GraphVisualisation = ({ setGraphState, graphState }) => {
   const [zoom, setZoom] = useState(1);
 
   const modalSize = {
-    width: Math.min(windowSize.width - 40, 900),
-    height: Math.min(windowSize.height - 40, 800),
+    width: Math.min(windowSize.width - 100, 800),
+    height: Math.min(windowSize.height - 200, 700),
   };
 
   const simulation = useRef(
